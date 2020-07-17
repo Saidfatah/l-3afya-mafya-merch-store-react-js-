757dbd7a24f249f3c83128ecd91a8b87dbb1fa5e
Saidfatah <said_designer@outlook.com>
2020-07-17 14:07:39 +0100

    adding context appi for produts

---
+++ src/Context/ProductsProvider.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import axios from 'axios'
+
+export const MyContext = React.createContext()
+function ProductsProvider(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductsProvider
