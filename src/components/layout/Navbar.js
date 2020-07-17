9d6476d28876b8c19a18e44423eee1e9210f3fb4
Saidfatah <said_designer@outlook.com>
2020-07-17 19:22:37 +0100

    media queries navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,21 +10,21 @@ const  Navbar=forwardRef((props,ref)=> {
        
         <div className="navBar">
             <div className="navBar__left">
-                <a className="navBar__Link sidemenu" onClick={e=> {ref.current.slideSideBarIn() }}><i className="fas fa-bars"></i>
-                </a>
-                 <Link className="navBar__Link" to="/shop">SHOP</Link>
-                 <Link className="navBar__Link" to="/collections">COLLECTIONS</Link>
-                 <Link className="navBar__Link" to="/contact">CONTACT</Link>
+                <a className="navBar__Link sidemenu" onClick={e=> {ref.current.slideSideBarIn() }}><i className="fas fa-bars"></i></a>
+                 <Link className="navBar__Link no-sidemenu" to="/shop">SHOP</Link>
+                 <Link className="navBar__Link no-sidemenu" to="/collections">COLLECTIONS</Link>
+                 <Link className="navBar__Link no-sidemenu" to="/contact">CONTACT</Link>
             </div>
             <div className="navBar__middle">
-                <Link className="navBar__Link noborder" to="/">
+                <Link className="navBar__Link logo sidemenu noborder" to="/">
                     <img className="navBar__logo" src={logo}/>
                 </Link>
             </div>
             <div className="navBar__right">
-               <Link className="navBar__Link" to="/account">Account</Link>
-               <a className="navBar__Link">Search</a>
-               <a className="navBar__Link" onClick={e=> {ref.current.slideCartIn() }}>Cart(<span>1</span>)</a>
+               <a className="navBar__Link sidemenu" onClick={e=> {ref.current.slideCartIn() }}><i class="fas fa-shopping-bag"></i></a>
+               <Link className="navBar__Link no-sidemenu" to="/account">Account</Link>
+               <a className="navBar__Link no-sidemenu">Search</a>
+               <a className="navBar__Link no-sidemenu" onClick={e=> {ref.current.slideCartIn() }}>Cart(<span>1</span>)</a>
             </div>
         </div>
     )
