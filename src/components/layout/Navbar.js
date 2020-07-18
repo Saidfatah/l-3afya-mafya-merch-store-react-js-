48c42d424b536af59a81e7265f5b1ec2557479e7
Saidfatah <said_designer@outlook.com>
2020-07-18 19:12:57 +0100

    fixed media querie for navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,18 +10,18 @@ const  Navbar=forwardRef((props,ref)=> {
        
         <div className="navBar">
             <div className="navBar__left">
-                <a className="navBar__Link sidemenu" onClick={e=> {ref.current.slideSideBarIn() }}><i className="fas fa-bars"></i></a>
+                <a className="navBar__Link sidemenu noHide" onClick={e=> {ref.current.slideSideBarIn() }}><i className="fas fa-bars"></i></a>
                  <Link className="navBar__Link no-sidemenu" to="/shop">SHOP</Link>
                  <Link className="navBar__Link no-sidemenu" to="/collections">COLLECTIONS</Link>
                  <Link className="navBar__Link no-sidemenu" to="/contact">CONTACT</Link>
             </div>
             <div className="navBar__middle">
-                <Link className="navBar__Link logo sidemenu noborder" to="/">
+                <Link className="navBar__Link noHide  noborder" to="/">
                     <img className="navBar__logo" src={logo}/>
                 </Link>
             </div>
             <div className="navBar__right">
-               <a className="navBar__Link sidemenu" onClick={e=> {ref.current.slideCartIn() }}><i class="fas fa-shopping-bag"></i></a>
+               <a className="navBar__Link sidemenu noHide" onClick={e=> {ref.current.slideCartIn() }}><i class="fas fa-shopping-bag"></i></a>
                <Link className="navBar__Link no-sidemenu" to="/account">Account</Link>
                <a className="navBar__Link no-sidemenu">Search</a>
                <a className="navBar__Link no-sidemenu" onClick={e=> {ref.current.slideCartIn() }}>Cart(<span>1</span>)</a>
