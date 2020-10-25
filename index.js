ef814fe047fa337eaf13b6f143b52bab69ef68d4
SaidFatah <said_designer@outlook.com>
2020-10-25 22:25:55 +0100

    update

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ app.use('/stripe',stripe)
 
 
 if(process.env.NODE_ENV == 'production'){
-   app.use(express.static('/dist'))
+    console('-------------------- production---------------------')
+   app.use(express.static('dist'))
    app.get('*',(req,res)=>{
        res.sendFile(path.resolve(__dirname,'dist','index.html'))
    })
